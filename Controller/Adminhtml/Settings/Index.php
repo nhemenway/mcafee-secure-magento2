3845949d0b1e25f53994ece91a66ff7cc91d6c11
Dennis Suratna <dennis@pathdefender.com>
2016-01-20 16:47:14 -0800

    McAfee SECURE Magento 2 Module Init

---
+++ Controller/Adminhtml/Settings/Index.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace McAfeeSecure\McAfeeSecure\Controller\Adminhtml\Settings;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+
+	protected $resultPageFactory;
+
+	public function __construct(Context $context, PageFactory $resultPageFactory)
+	{
+		parent::__construct($context);
+		$this->resultPageFactory = $resultPageFactory;
+	}
+
+	public function execute()
+	{
+		$resultPage = $this->resultPageFactory->create();
+		$resultPage->setActiveMenu('McAfeeSecure_McAfeeSecure::settings');
+		$resultPage->addBreadcrumb(__('McAfee SECURE'), __('McAfee SECURE'));
+		$resultPage->addBreadcrumb(__('Settings'), __('Settings'));
+		$resultPage->getConfig()->getTitle()->prepend(__('McAfee SECURE Settings'));
+
+		return $resultPage;
+	}
+
+	protected function _isAllowed()
+	{
+		return true;
+	}
+}
