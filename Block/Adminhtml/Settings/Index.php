7da0b61ca2aac1540589ab00ac744c89faad5a8f
Patrick Austin <patrickaustin@Patrick-Austins-iMac.local>
2019-05-13 09:40:32 -0700

    Release version 0.0.7

--- Block/Adminhtml/Settings/Index.php
+++ Block/Adminhtml/Settings/Index.php
@@ -7,25 +7,25 @@ use Magento\Backend\Block\Template;
 class Index extends Template
 {
 
-	protected $backendAuthSession;
+    protected $backendAuthSession;
 
-	public function __construct(Template\Context $context, \Magento\Backend\Model\Auth\Session $backendAuthSession, array $data = [])
-	{
-		$this->backendAuthSession = $backendAuthSession;
-		parent::__construct($context, $data);
-	}
+    public function __construct(Template\Context $context, \Magento\Backend\Model\Auth\Session $backendAuthSession, array $data = [])
+    {
+        $this->backendAuthSession = $backendAuthSession;
+        parent::__construct($context, $data);
+    }
 
-	public function getHost()
-	{
-		return $this->_storeManager->getStore()->getBaseUrl();
-	}
+    public function getHost()
+    {
+        return $this->_storeManager->getStore()->getBaseUrl();
+    }
 
-	public function getUserEmail()
-	{
-		if ($this->backendAuthSession->isLoggedIn()) {
-			return $this->backendAuthSession->getUser()->getEmail();
-		}
+    public function getUserEmail()
+    {
+        if ($this->backendAuthSession->isLoggedIn()) {
+            return $this->backendAuthSession->getUser()->getEmail();
+        }
 
-		return "";
-	}
+        return "";
+    }
 }
--- Controller/Adminhtml/Settings/Index.php
+++ Controller/Adminhtml/Settings/Index.php
@@ -8,27 +8,27 @@ use Magento\Framework\View\Result\PageFactory;
 class Index extends \Magento\Backend\App\Action
 {
 
-	protected $resultPageFactory;
+    protected $resultPageFactory;
 
-	public function __construct(Context $context, PageFactory $resultPageFactory)
-	{
-		parent::__construct($context);
-		$this->resultPageFactory = $resultPageFactory;
-	}
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
 
-	public function execute()
-	{
-		$resultPage = $this->resultPageFactory->create();
-		$resultPage->setActiveMenu('McAfeeSecure_McAfeeSecure::settings');
-		$resultPage->addBreadcrumb(__('McAfee SECURE'), __('McAfee SECURE'));
-		$resultPage->addBreadcrumb(__('Settings'), __('Settings'));
-		$resultPage->getConfig()->getTitle()->prepend(__('McAfee SECURE Settings'));
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('McAfeeSecure_McAfeeSecure::settings');
+        $resultPage->addBreadcrumb(__('McAfee SECURE'), __('McAfee SECURE'));
+        $resultPage->addBreadcrumb(__('Settings'), __('Settings'));
+        $resultPage->getConfig()->getTitle()->prepend(__('McAfee SECURE Settings'));
 
-		return $resultPage;
-	}
+        return $resultPage;
+    }
 
-	protected function _isAllowed()
-	{
-		return $this->_authorization->isAllowed('McAfeeSecure_McAfeeSecure::settings');
-	}
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('McAfeeSecure_McAfeeSecure::settings');
+    }
 }
